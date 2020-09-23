73e76c327f65d1bf3620fec947ffa56adb76b0e8
jarvis <jarvisrizo_southsystem@getnet.br>
2020-09-23 00:13:47 -0300

    iniciando projeto

---
+++ src/main/java/com/iscob/iscob/IscobApplication.java
@@ -0,0 +1,13 @@
+package com.iscob.iscob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IscobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iscob/iscob/IscobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iscob.iscob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IscobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
